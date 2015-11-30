3070b4d249373e536c1ec55c32823d576d1d2eeb
Guilherme Bernal <guilherme@localhost.localdomain>
2015-11-30 10:51:42 -0300

    Initialize the SQLite connector on main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <cloudprovider/CloudProvider.hpp>
+#include <Poco/Data/SQLite/Connector.h>
 
 int main() {
+    Poco::Data::SQLite::Connector::registerConnector();
     CloudProvider::createDatabase("dropbox-test.db");
 }
